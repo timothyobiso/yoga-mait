8d4d09e572c46d05608da2f758d1eb493961d9ae
bsoli <benjamintsoli@gmail.com>
2023-05-02 16:11:42 -0400

    update to query_classifer.py and csv file to use for query classification

--- query_classifier.py
+++ query_classifier.py
@@ -1,12 +1,27 @@
 import json 
 import pandas as pd
 import numpy as np
+from sentence_transformers import SentenceTransformer
 
 
+
+
+# gets embedding model for query classification
+def get_model():
+    return SentenceTransformer('all-MiniLM-L6-v2')
+
+# gets dataframe used for cosine similarity classification
+def get_classifier_df() -> pd.DataFrame:
+    model = get_model()
+    with open('classifer_data.csv', 'r', encoding='utf8') as data:
+        data_df = pd.read_csv(data)
+    data_df['embedding'] = [embedding for embedding in model.encode(data_df['text'].tolist())]
+    return data_df
 def cos_sim(l1: np.array, l2: np.array):
     return np.dot(l1, l2) / (np.linalg.norm(l1) * np.linalg.norm(l2))
 
-def classify(query: str, df: pd.DataFrame):
-    idx = np.argmax([cos_sim(query, emb) for emb in df['embedding'].tolist()])
+def classify(query_emb , df: pd.DataFrame):
+    decoder = {2: 'names', 0: 'description', 1: 'benefits'}
+    idx = np.argmax([cos_sim(query_emb, emb) for emb in df['embedding'].tolist()])
     label = df.iloc[idx]['label']
     return label
