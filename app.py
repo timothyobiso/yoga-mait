64cc1ccab123c88cb9a2afa08a1f33321614a9ef
Timothy Obiso <9258849+timothyobiso@users.noreply.github.com>
2023-05-09 14:25:05 -0400

    commented

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+# Main Flask File
+# Developed by Timothy Obiso
+
+# Import Statements
 from flask import Flask, render_template, request
 from flask_caching import Cache
 from utils import get_pose, string_to_list
@@ -6,62 +10,91 @@ from gpt_yoga_interface import query_to_prompt, ask_chat_gpt
 from es_search import SearchIndex
 from query_classifier import get_model, get_classifier_df, classify
 
+# App Setup
 app = Flask(__name__)
-app.jinja_env.filters['zip'] = zip
+app.jinja_env.filters['zip'] = zip #allows use of zip on flask templates
 
+# Cache Setup
 cache = Cache(config={'CACHE_TYPE': "SimpleCache"})
 cache.init_app(app)
 
+# Query Classifier Setup
 MODEL = get_model()
 DF = get_classifier_df()
 
+# Home/Search Page
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return render_template("home.html")
 
+# 404 Error Page
 @app.errorhandler(404)
 def not_found(e):
     return render_template("404.html")
 
+# Results Page
 @app.route("/results", methods=["POST"])
 def results():
+    # Connect to Elasticsearch Database
     connections.create_connection(hosts=["localhost"], timeout=100, alias="default")
+    # Get Query from page
     query_text = request.form["query"]
-    # print(query_text)
+
+    # Fix for " pose"
     if query_text[len(query_text)-5:] == " pose":
         query_text = query_text[:len(query_text)-5]
+
+    # Classify Query (name, benefits, description)
     cls = classify(MODEL.encode(query_text), DF)
 
-    print("CLASSIFIED AS:", cls)
+    # Uncomment to see classification
+    # print("CLASSIFIED AS:", cls)
 
+    # Classified as "benefits" or "description"
     if cls != "name":
         res = []
+        # send to GPT with prompt engineering wrapper
+        # Turn GPT String into list
         gpt_results = string_to_list(ask_chat_gpt(query_to_prompt(query_text)))
+
+        # Iterate through GPT responses
         for r in gpt_results:
+            # Use Elasticsearch to get poses
             response, fail = SearchIndex.search_index(r, cls)
+
+            # Top 5 poses for each GPT result
             for p in response[:5]:
                 if p not in res:
                     res.append(p)
-    else:
 
+    # Classified as "name"
+    else:
+        # res = results, fail = True if name keyword search failed, False otherwise
         res, fail = SearchIndex.search_index(query_text, cls)
-    # gpt_results = string_to_list(ask_chat_gpt(query_to_prompt(query_text)))
+
+    # Uncomment to print results to be shown on results page
     # print(res)
 
+    # Update cache
     cache.set("q", query_text)
-    return render_template("results.html",
-                           q=query_text, page=1, results=res, fail=fail, cls=cls)
+
+    #
+    return render_template("results.html", q=query_text, results=res, fail=fail, cls=cls)
 
 
+# Pose Page
 @app.route('/pose/<name>')
 def pose(name):
+    # Get Picture of Pose
     image_url = "/static/images/"+name+".png"
+
+    # Get Query From Cache to persist to user
     q = cache.get("q")
     filler = "" if q is None else q
     return render_template("pose.html",
                            pose=get_pose(name),
-                           anchor=name, image=image_url, q=filler) # image=image_urls[is_image(image_urls)]
+                           anchor=name, image=image_url, q=filler)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, use_debugger=True, use_reloader=True)
+    app.run()
--- utils.py
+++ utils.py
@@ -1,11 +1,19 @@
+# "Utility" Functions
+# Developed by Timothy Obiso
 from typing import Dict
 import json
 
 
+# Return pose information as Dict from JSON
 def get_pose(anchor: str) -> Dict:
     with open('data/data_updated.json') as f:
         data = json.load(f)
         return data[anchor]
+
+# Turn ChatGPT Output (a string formatted as
+# 1. Result
+# 2. Result
+# 3. Result...) into a list
 
 def string_to_list(string):
     lines = string.strip().split('\n')
